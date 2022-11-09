7c0e9228269c6b3589dd0bd57261944cdccf69a1
Levy Monteiro <levymonteiro268@gmail.com>
2022-11-09 18:10:06 -0300

    update emmiter.js

--- src/emitter.js
+++ src/emitter.js
@@ -1,6 +1,24 @@
 const emitter = {
+    events: {},
 
+    on(event, cb) {
+        emitter.events[event] = emitter.events[event] || []
+        emitter.events[event].push(cb)
+    },
+
+    emit(event) {
+        if(event in emitter.events === false)
+        return;
+        
+        emitter.events[event].forEach((e) => {
+            e()
+        })
+    }
 }
-console.log("Emitter")
+
+emitter.on('click', () => console.log('cliquei'))
+emitter.on('click', () => console.log('cliquei 2'))
+
+emitter.emit('click')
 
 export {emitter}
